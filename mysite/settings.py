b039abb79fb67510db25bd4bd7ab973f9daa8745
icate95 <ianesellicaterina@gmail.com>
2022-01-30 17:52:40 +0100

    nsba

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(m8ey&1o#7r$od83o$hp#a_i@_no(#t5l)7akyy3ikhr4o5%-('
 
